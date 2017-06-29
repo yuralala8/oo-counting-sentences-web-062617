96c350a978b515faa64ff2a4b984e1b88484f456
yuralala8 <yc1435@nyu.edu>
2017-06-29 09:15:59 -0400

    oo-counting-sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
   end
+end
 
   def question?
-
-  end
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
   end
+end
 
-  def count_sentences
+def count_sentences
+    self.squeeze.split(/[.!?]/).count
+end
 
-  end
 end
